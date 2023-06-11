980260fc3429c49bd549694460b91d5efead6a0c
Lee TB <tblyduc2412@gmail.com>
2023-06-11 18:37:46 +0700

    Header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,6 +16,7 @@ const Nav = () => {
             activeClass="active"
             smooth={true}
             spy={true}
+            offset={-200}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BiHomeAlt />
@@ -52,6 +53,7 @@ const Nav = () => {
             activeClass="active"
             smooth={true}
             spy={true}
+            offset={50}
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsChatSquareText />
